b0c02d66151b6d325f7a6b719475cb76ef93b320
digiur <digiur@digiur.dev>
2024-09-01 10:53:04 -0600

    Add Install Script

---
+++ install.sh
@@ -0,0 +1,318 @@
+#!/usr/bin/bash
+#
+#   This script installs DigiurOS to your system.
+#   Usage:
+#       $ wget -qO- https://get.casaos.io/ | bash
+
+#   This only work on  Linux systems. Please
+#   open an issue if you notice any bugs.
+#
+clear
+echo -e "\e[0m\c"
+
+echo '
+  ____                         ____   _____ 
+ |  __ \                      / __ \ / ____|
+ | |  \ \ _   _   _ _  _  __ | |  | | (___  
+ | |   | |_|/ _ \|_| || |/ _\| |  | |\___ \ 
+ | |__/ /| | (_| | | || | |  | |__| |____) |
+ |_____/ |_|__  /|_|\_,_|_|   \____/|_____/ 
+          |____/                            
+'
+
+export PATH=/usr/sbin:$PATH
+export DEBIAN_FRONTEND=noninteractive
+
+set -e
+
+###############################################################################
+# GOLBALS                                                                     #
+###############################################################################
+
+((EUID)) && sudo_cmd="sudo"
+
+# shellcheck source=/dev/null
+source /etc/os-release
+
+# SYSTEM REQUIREMENTS
+readonly MINIMUM_DOCKER_VERSION="20"
+readonly DEPEND_PACKAGES=('wget' 'curl' 'smartmontools' 'parted' 'ntfs-3g' 'net-tools' 'udevil' 'samba' 'cifs-utils' 'mergerfs' 'unzip')
+readonly DEPEND_COMMANDS=('wget' 'curl' 'smartctl' 'parted' 'ntfs-3g' 'netstat' 'udevil' 'smbd' 'mount.cifs' 'mount.mergerfs' 'unzip')
+
+# SYSTEM INFO
+PHYSICAL_MEMORY=$(LC_ALL=C free -m | awk '/Mem:/ { print $2 }')
+readonly PHYSICAL_MEMORY
+
+FREE_DISK_BYTES=$(LC_ALL=C df -P / | tail -n 1 | awk '{print $4}')
+readonly FREE_DISK_BYTES
+
+readonly FREE_DISK_GB=$((FREE_DISK_BYTES / 1024 / 1024))
+
+LSB_DIST=$( ([ -n "${ID_LIKE}" ] && echo "${ID_LIKE}") || ([ -n "${ID}" ] && echo "${ID}"))
+readonly LSB_DIST
+
+DIST=$(echo "${ID}")
+readonly DIST
+
+UNAME_M="$(uname -m)"
+readonly UNAME_M
+
+UNAME_U="$(uname -s)"
+readonly UNAME_U
+
+# COLORS
+readonly COLOUR_RESET='\e[0m'
+readonly aCOLOUR=(
+    '\e[38;5;154m' # green  	| Lines, bullets and separators
+    '\e[1m'        # Bold white	| Main descriptions
+    '\e[90m'       # Grey		| Credits
+    '\e[91m'       # Red		| Update notifications Alert
+    '\e[33m'       # Yellow		| Emphasis
+)
+
+readonly GREEN_LINE=" ${aCOLOUR[0]}─────────────────────────────────────────────────────$COLOUR_RESET"
+readonly GREEN_BULLET=" ${aCOLOUR[0]}-$COLOUR_RESET"
+readonly GREEN_SEPARATOR="${aCOLOUR[0]}:$COLOUR_RESET"
+
+# CASAOS VARIABLES
+TARGET_ARCH=""
+TMP_ROOT=/tmp/casaos-installer
+REGION="UNKNOWN"
+CASA_DOWNLOAD_DOMAIN="https://github.com/"
+
+trap 'onCtrlC' INT
+onCtrlC() {
+    echo -e "${COLOUR_RESET}"
+    exit 1
+}
+
+###############################################################################
+# Helpers                                                                     #
+###############################################################################
+
+#######################################
+# Custom printing function
+# Globals:
+#   None
+# Arguments:
+#   $1 0:OK   1:FAILED  2:INFO  3:NOTICE
+#   message
+# Returns:
+#   None
+#######################################
+
+Show() {
+    # OK
+    if (($1 == 0)); then
+        echo -e "${aCOLOUR[2]}[$COLOUR_RESET${aCOLOUR[0]}  OK  $COLOUR_RESET${aCOLOUR[2]}]$COLOUR_RESET $2"
+    # FAILED
+    elif (($1 == 1)); then
+        echo -e "${aCOLOUR[2]}[$COLOUR_RESET${aCOLOUR[3]}FAILED$COLOUR_RESET${aCOLOUR[2]}]$COLOUR_RESET $2"
+        exit 1
+    # INFO
+    elif (($1 == 2)); then
+        echo -e "${aCOLOUR[2]}[$COLOUR_RESET${aCOLOUR[0]} INFO $COLOUR_RESET${aCOLOUR[2]}]$COLOUR_RESET $2"
+    # NOTICE
+    elif (($1 == 3)); then
+        echo -e "${aCOLOUR[2]}[$COLOUR_RESET${aCOLOUR[4]}NOTICE$COLOUR_RESET${aCOLOUR[2]}]$COLOUR_RESET $2"
+    fi
+}
+
+GreyStart() {
+    echo -e "${aCOLOUR[2]}\c"
+}
+
+ColorReset() {
+    echo -e "$COLOUR_RESET\c"
+}
+
+# Clear Terminal
+Clear_Term() {
+
+    # Without an input terminal, there is no point in doing this.
+    [[ -t 0 ]] || return
+
+    # Printing terminal height - 1 newlines seems to be the fastest method that is compatible with all terminal types.
+    lines=$(tput lines) i newlines
+    local lines
+
+    for ((i = 1; i < ${lines% *}; i++)); do newlines+='\n'; done
+    echo -ne "\e[0m$newlines\e[H"
+
+}
+
+###############################################################################
+# Install Package Dependencies                                                #
+###############################################################################
+Update_Package_Resource() {
+    Show 2 "Updating package manager..."
+    GreyStart
+    ${sudo_cmd} apt-get update -qq
+    ColorReset
+    Show 0 "Update package manager complete."
+}
+
+Install_Depends() {
+    for ((i = 0; i < ${#DEPEND_COMMANDS[@]}; i++)); do
+        cmd=${DEPEND_COMMANDS[i]}
+        if [[ ! -x $(${sudo_cmd} which "$cmd") ]]; then
+            packageNeeded=${DEPEND_PACKAGES[i]}
+            Show 2 "Install the necessary dependency: \e[33m$packageNeeded \e[0m"
+            GreyStart
+            ${sudo_cmd} apt-get -y -qq install "$packageNeeded" --no-upgrade
+            ColorReset
+        fi
+    done
+}
+
+Check_Dependency_Installation() {
+    for ((i = 0; i < ${#DEPEND_COMMANDS[@]}; i++)); do
+        cmd=${DEPEND_COMMANDS[i]}
+        if [[ ! -x $(${sudo_cmd} which "$cmd") ]]; then
+            packageNeeded=${DEPEND_PACKAGES[i]}
+            Show 1 "Dependency \e[33m$packageNeeded \e[0m installation failed, please try again manually!"
+            exit 1
+        fi
+    done
+}
+
+###############################################################################
+# Install Docker                                                              #
+###############################################################################
+Check_Docker_Install() {
+    if [[ -x "$(command -v docker)" ]]; then
+        Docker_Version=$(${sudo_cmd} docker version --format '{{.Server.Version}}')
+        if [[ $? -ne 0 ]]; then
+            Install_Docker
+        elif [[ ${Docker_Version:0:2} -lt "${MINIMUM_DOCKER_VERSION}" ]]; then
+            Show 1 "Recommended minimum Docker version is \e[33m${MINIMUM_DOCKER_VERSION}.xx.xx\e[0m,\Current Docker version is \e[33m${Docker_Version}\e[0m,\nPlease uninstall current Docker and rerun the CasaOS installation script."
+            exit 1
+        else
+            Show 0 "Current Docker version is ${Docker_Version}."
+        fi
+    else
+        Install_Docker
+    fi
+}
+
+Install_Docker() {
+    Show 2 "Install the necessary dependencies: \e[33mDocker \e[0m"
+    if [[ ! -d "${PREFIX}/etc/apt/sources.list.d" ]]; then
+        ${sudo_cmd} mkdir -p "${PREFIX}/etc/apt/sources.list.d"
+    fi
+    GreyStart
+    ${sudo_cmd} curl -fsSL https://get.docker.com | bash
+    ColorReset
+    if [[ $? -ne 0 ]]; then
+        Show 1 "Installation failed, please try again."
+        exit 1
+    else
+        Check_Docker_Install_Final
+    fi
+}
+
+Check_Docker_Install_Final() {
+    if [[ -x "$(command -v docker)" ]]; then
+        Docker_Version=$(${sudo_cmd} docker version --format '{{.Server.Version}}')
+        if [[ $? -ne 0 ]]; then
+            Install_Docker
+        elif [[ ${Docker_Version:0:2} -lt "${MINIMUM_DOCKER_VERSION}" ]]; then
+            Show 1 "Recommended minimum Docker version is \e[33m${MINIMUM_DOCKER_VERSION}.xx.xx\e[0m,\Current Docker version is \e[33m${Docker_Version}\e[0m,\nPlease uninstall current Docker and rerun the CasaOS installation script."
+            exit 1
+        else
+            Show 0 "Current Docker version is ${Docker_Version}."
+            Check_Docker_Running
+        fi
+    else
+        Show 1 "Installation failed, please run 'curl -fsSL https://get.docker.com | bash' and rerun the CasaOS installation script."
+        exit 1
+    fi
+}
+
+Check_Docker_Running() {
+    for ((i = 1; i <= 3; i++)); do
+        sleep 3
+        if [[ ! $(${sudo_cmd} systemctl is-active docker) == "active" ]]; then
+            Show 1 "Docker is not running, try to start"
+            ${sudo_cmd} systemctl start docker
+        else
+            break
+        fi
+    done
+}
+
+###############################################################################
+# Welcome Helpers                                                             #
+###############################################################################
+Get_IPs() {
+    PORT=$(${sudo_cmd} cat ${CASA_CONF_PATH} | grep port | sed 's/port=//')
+    ALL_NIC=$($sudo_cmd ls /sys/class/net/ | grep -v "$(ls /sys/devices/virtual/net/)")
+    for NIC in ${ALL_NIC}; do
+        IP=$($sudo_cmd ifconfig "${NIC}" | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | sed -e 's/addr://g')
+        if [[ -n $IP ]]; then
+            if [[ "$PORT" -eq "80" ]]; then
+                echo -e "${GREEN_BULLET} http://$IP (${NIC})"
+            else
+                echo -e "${GREEN_BULLET} http://$IP:$PORT (${NIC})"
+            fi
+        fi
+    done
+}
+
+Welcome_Banner() {
+    CASA_TAG=$(casaos -v)
+
+    echo -e "${GREEN_LINE}${aCOLOUR[1]}"
+    echo -e " CasaOS ${CASA_TAG}${COLOUR_RESET} is running at${COLOUR_RESET}${GREEN_SEPARATOR}"
+    echo -e "${GREEN_LINE}"
+    Get_IPs
+    echo -e " Open your browser and visit the above address."
+    echo -e "${GREEN_LINE}"
+    echo -e ""
+    echo -e " ${aCOLOUR[2]}CasaOS Project  : https://github.com/IceWhaleTech/CasaOS"
+    echo -e " ${aCOLOUR[2]}CasaOS Team     : https://github.com/IceWhaleTech/CasaOS#maintainers"
+    echo -e " ${aCOLOUR[2]}CasaOS Discord  : https://discord.gg/knqAbbBbeX"
+    echo -e " ${aCOLOUR[2]}Website         : https://www.casaos.io"
+    echo -e " ${aCOLOUR[2]}Online Demo     : http://demo.casaos.io"
+    echo -e ""
+    echo -e " ${COLOUR_RESET}${aCOLOUR[1]}Uninstall       ${COLOUR_RESET}: casaos-uninstall"
+    echo -e "${COLOUR_RESET}"
+}
+
+###############################################################################
+# Main                                                                        #
+###############################################################################
+usage() {
+    cat <<-EOF
+		Usage: install.sh [options]
+		Valid options are:
+		    -p <build_dir>          Specify build directory (Local install)
+		    -h                      Show this help message and exit
+	EOF
+    exit "$1"
+}
+
+while getopts ":p:h" arg; do
+    case "$arg" in
+    p)
+        BUILD_DIR=$OPTARG
+        ;;
+    h)
+        usage 0
+        ;;
+    *)
+        usage 1
+        ;;
+    esac
+done
+
+echo "Step 1: Install Depends"
+Update_Package_Resource
+Install_Depends
+Check_Dependency_Installation
+
+echo "Step 2: Check And Install Docker"
+Check_Docker_Install
+
+echo "Step 3: Clear Term and Show Welcome Banner"
+Welcome_Banner
