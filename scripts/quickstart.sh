dd3827d8a5d0df15ba10d667ae25c8167c968c74
digiur <digiur@digiur.dev>
2024-09-02 08:09:26 -0600

    oof

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -6,5 +6,4 @@ sudo usermod -aG docker $USER
 getent group docker
 git clone https://github.com/digiur/digiur-net.git
 chmod +x ./digiur-net/scripts/install.sh
-echo woot
 sg docker -c ./digiur-net/scripts/install.sh
