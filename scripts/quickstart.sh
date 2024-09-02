03cdf5620cc287e5b75e1aa84c0a70465fb4dcce
digiur <digiur@digiur.dev>
2024-09-02 08:08:09 -0600

    clone?

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -4,6 +4,7 @@ groups
 getent group docker
 sudo usermod -aG docker $USER
 getent group docker
-groups
+git clone https://github.com/digiur/digiur-net.git
 chmod +x ./digiur-net/scripts/install.sh
+echo woot
 sg docker -c ./digiur-net/scripts/install.sh
