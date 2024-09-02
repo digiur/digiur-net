3aeb68e96e4e87dff8400c30f67d13b5577708a1
digiur <digiur@digiur.dev>
2024-09-01 21:39:56 -0600

    more

--- install.sh
+++ install.sh
@@ -200,9 +200,14 @@ Install_Docker() {
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
         $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    ColorReset
+
+    Show 2 "Install Packages..."
+    GreyStart
     sudo apt-get update -qq
     sudo apt-get -y -qq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     ColorReset
+
     Show 0 "Docker install complete."
 }
 
