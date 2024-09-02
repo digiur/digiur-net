37b88ac2e5269cb5c76d0cce93460ef3dad512fb
digiur <digiur@digiur.dev>
2024-09-01 23:05:52 -0600

    bash instead

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -6,4 +6,4 @@ sudo usermod -aG docker $USER
 getent group docker
 echo -n "Ready to install. Press any key to continue..."
 read -n 1 -s
-sg docker -c ./digiur-net/scripts/install.sh
+bash -c ./digiur-net/scripts/install.sh
